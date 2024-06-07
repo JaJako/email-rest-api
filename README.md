c95be726c3340427570b7db7df2ee574f49eddd7
Jan Jakobus <jan.jakobus@studmail.w-hs.de>
2024-06-07 19:51:30 +0200

    Creates Spring Boot maven project template

---
+++ README.md
@@ -0,0 +1,3 @@
+# email-rest-api
+
+A simple CRUD REST service in context of an email server.
---
+++ src/test/java/de/jjakobus/emailrestapi/EmailRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jjakobus.emailrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailRestApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
